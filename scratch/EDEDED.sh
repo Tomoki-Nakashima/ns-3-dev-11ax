23503895311989b0c131e41f737682d0d3b0c0af
TomokiNAKASHIMA <tomoki_nakasima@yahoo.co.jp>
2021-02-03 10:56:51 +0900

    update: script
    1000 ite only downlink mcs0

--- scratch/EDEDED.sh
+++ scratch/EDEDED.sh
@@ -22,9 +22,9 @@ primaryChannelBssE=36
 primaryChannelBssF=144
 primaryChannelBssG=144
 
-mcs1=VhtMcs8
-mcs2=VhtMcs8
-mcs3=VhtMcs8
+mcs1=VhtMcs0
+mcs2=VhtMcs0
+mcs3=VhtMcs0
 mcs4=VhtMcs0
 mcs5=VhtMcs0
 mcs6=VhtMcs0
@@ -59,51 +59,6 @@ downlinkG=0
 uplinkA=0
 uplinkB=0
 uplinkC=0
-uplinkD=100
-uplinkE=100
-uplinkF=100
-uplinkG=0
-
-channelBondingType="ConstantThreshold"
-Test=""
-
-n=50
-nBss=6
-
-rxSensitivity=-100
-
-result_filename=EDEDED_50sta.csv
-position_filename=EDEDED_position_50sta.csv
-
-for ccaSdThreshold in -62;
-do
-for x in {0..19};
-do
-for RngRun in {1..5};
-do
-RngRun=$((RngRun + x * 5))
-echo "Starting Bss=$nBss n=$n, ccaSdThreshold=$ccaSdThreshold, distance=$distance, RngRun=$RngRun"
-../waf --run "channel-bonding  --payloadSize=$payloadSize --simulationTime=$simulationTime --distance=$distance --interBssDistance=$interBssDistance --txMaskInnerBandMinimumRejection=$txMaskInnerBandMinimumRejection --txMaskOuterBandMinimumRejection=$txMaskOuterBandMinimumRejection --txMaskOuterBandMaximumRejection=$txMaskOuterBandMaximumRejection --channelBssA=$channelBssA --primaryChannelBssA=$primaryChannelBssA --channelBssB=$channelBssB --primaryChannelBssB=$primaryChannelBssB --channelBssC=$channelBssC --primaryChannelBssC=$primaryChannelBssC --channelBssD=$channelBssD --primaryChannelBssD=$primaryChannelBssD --channelBssE=$channelBssE --primaryChannelBssE=$primaryChannelBssE --channelBssF=$channelBssF --primaryChannelBssF=$primaryChannelBssF --channelBssG=$channelBssG --primaryChannelBssG=$primaryChannelBssG --mcs1=$mcs1 --mcs2=$mcs2 --mcs3=$mcs3 --mcs4=$mcs4 --mcs5=$mcs5 --mcs6=$mcs6 --mcs7=$mcs7 --ccaSdThreshold=$ccaSdThreshold --ccaEdThresholdPrimaryBssA=${ccaEdThresholdPrimaryBssA} --ccaEdThresholdSecondaryBssA=$ccaEdThresholdSecondaryBssA --ccaEdThresholdPrimaryBssB=${ccaEdThresholdPrimaryBssB} --ccaEdThresholdSecondaryBssB=$ccaEdThresholdSecondaryBssB --ccaEdThresholdPrimaryBssC=${ccaEdThresholdPrimaryBssC} --ccaEdThresholdSecondaryBssC=$ccaEdThresholdSecondaryBssC  --ccaEdThresholdPrimaryBssD=${ccaEdThresholdPrimaryBssD} --ccaEdThresholdSecondaryBssD=$ccaEdThresholdSecondaryBssD --ccaEdThresholdPrimaryBssE=${ccaEdThresholdPrimaryBssE} --ccaEdThresholdSecondaryBssE=$ccaEdThresholdSecondaryBssE --ccaEdThresholdPrimaryBssF=${ccaEdThresholdPrimaryBssF} --ccaEdThresholdSecondaryBssF=$ccaEdThresholdSecondaryBssF --ccaEdThresholdPrimaryBssG=${ccaEdThresholdPrimaryBssG} --ccaEdThresholdSecondaryBssG=$ccaEdThresholdSecondaryBssG --downlinkA=${downlinkA} --downlinkB=$downlinkB --downlinkC=$downlinkC --downlinkD=$downlinkD --downlinkE=$downlinkE --downlinkF=$downlinkF --downlinkG=$downlinkG --uplinkA=$uplinkA --uplinkB=$uplinkB --uplinkC=$uplinkC --uplinkD=${uplinkD} --uplinkE=$uplinkE --uplinkF=$uplinkF --uplinkG=$uplinkG --channelBondingType=$channelBondingType --n=$n --nBss=$nBss --rxSensitivity=$rxSensitivity --RngRun=$RngRun --result_filename=$result_filename --position_filename=$position_filename" &
-done
-wait
-done
-wait
-done
-
-
-
-mcs1=VhtMcs8
-mcs2=VhtMcs8
-mcs3=VhtMcs8
-mcs4=VhtMcs8
-mcs5=VhtMcs8
-mcs6=VhtMcs8
-mcs7=VhtMcs0
-
-
-uplinkA=0
-uplinkB=0
-uplinkC=0
 uplinkD=0
 uplinkE=0
 uplinkF=0
@@ -117,8 +72,8 @@ nBss=6
 
 rxSensitivity=-100
 
-result_filename=EDEDED_onlydown.csv
-position_filename=EDEDED_position_onlydown.csv
+result_filename=EDEDED.csv
+position_filename=EDEDED_position.csv
 
 for ccaSdThreshold in -62;
 do
