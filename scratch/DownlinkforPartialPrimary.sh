29eac211e33b073a32aeb8f79ba86d3a67ec8a68
TomokiNAKASHIMA <tomoki_nakasima@yahoo.co.jp>
2021-02-10 11:31:14 +0900

    create all scrach for bachelor research

---
+++ scratch/DownlinkforPartialPrimary.sh
@@ -0,0 +1,90 @@
+payloadSize=1472 # bytes
+simulationTime=10 # seconds
+distance=5 # meters
+interBssDistance=10 # meters
+txMaskInnerBandMinimumRejection=-40.0 # dBr
+txMaskOuterBandMinimumRejection=-56.0 # dBr
+txMaskOuterBandMaximumRejection=-80.0 # dBr
+
+channelBssA=36
+channelBssB=36
+channelBssC=52
+channelBssD=52
+channelBssE=44
+channelBssF=44
+channelBssG=44
+
+primaryChannelBssA=36
+primaryChannelBssB=36
+primaryChannelBssC=52
+primaryChannelBssD=52
+primaryChannelBssE=44
+primaryChannelBssF=44
+primaryChannelBssG=44
+
+mcs1=VhtMcs0
+mcs2=VhtMcs0
+mcs3=VhtMcs0
+mcs4=VhtMcs0
+mcs5=VhtMcs0
+mcs6=VhtMcs0
+mcs7=VhtMcs0
+
+ccaSdThreshold=-82
+
+ccaEdThresholdPrimaryBssA=-62.0
+ccaEdThresholdSecondaryBssA=-62.0
+ccaEdThresholdPrimaryBssB=-62.0
+ccaEdThresholdSecondaryBssB=-62.0
+ccaEdThresholdPrimaryBssC=-62.0
+ccaEdThresholdSecondaryBssC=-62.0
+ccaEdThresholdPrimaryBssD=-62.0
+ccaEdThresholdSecondaryBssD=-62.0
+ccaEdThresholdPrimaryBssE=-62.0
+ccaEdThresholdSecondaryBssE=-62.0
+ccaEdThresholdPrimaryBssF=-62.0
+ccaEdThresholdSecondaryBssF=-62.0
+ccaEdThresholdPrimaryBssG=-62.0
+ccaEdThresholdSecondaryBssG=-62.0
+
+downlinkA=100
+downlinkB=100
+downlinkC=0
+downlinkD=0
+downlinkE=0
+downlinkF=0
+downlinkG=0
+
+
+uplinkA=0
+uplinkB=0
+uplinkC=0
+uplinkD=0
+uplinkE=0
+uplinkF=0
+uplinkG=0
+
+channelBondingType="ConstantThreshold"
+Test=""
+
+n=10
+nBss=4
+
+rxSensitivity=-100
+
+result_filename=DownlinkforPartialPrimary.csv
+
+for ccaSdThreshold in -82;
+do
+for mcs in VhtMcs0 VhtMcs1 VhtMcs2 VhtMcs3 VhtMcs4 VhtMcs5 VhtMcs6 VhtMcs7 VhtMcs8;
+do
+for RngRun in 1 2 3 4 5 6 7 8 9 10;
+do
+echo "Starting Bss=$nBss n=$n, ccaSdThreshold=$ccaSdThreshold, mcs=$mcs, distance=$distance "
+../waf --run "channel-bonding  --payloadSize=$payloadSize --simulationTime=$simulationTime --distance=$distance --interBssDistance=$interBssDistance --txMaskInnerBandMinimumRejection=$txMaskInnerBandMinimumRejection --txMaskOuterBandMinimumRejection=$txMaskOuterBandMinimumRejection --txMaskOuterBandMaximumRejection=$txMaskOuterBandMaximumRejection --channelBssA=$channelBssA --primaryChannelBssA=$primaryChannelBssA --channelBssB=$channelBssB --primaryChannelBssB=$primaryChannelBssB --channelBssC=$channelBssC --primaryChannelBssC=$primaryChannelBssC --channelBssD=$channelBssD --primaryChannelBssD=$primaryChannelBssD --channelBssE=$channelBssE --primaryChannelBssE=$primaryChannelBssE --channelBssF=$channelBssF --primaryChannelBssF=$primaryChannelBssF --channelBssG=$channelBssG --primaryChannelBssG=$primaryChannelBssG --mcs1=$mcs --mcs2=$mcs --mcs3=$mcs --mcs4=$mcs --mcs5=$mcs --mcs6=$mcs --mcs7=$mcs --ccaSdThreshold=$ccaSdThreshold --ccaEdThresholdPrimaryBssA=${ccaEdThresholdPrimaryBssA} --ccaEdThresholdSecondaryBssA=$ccaEdThresholdSecondaryBssA --ccaEdThresholdPrimaryBssB=${ccaEdThresholdPrimaryBssB} --ccaEdThresholdSecondaryBssB=$ccaEdThresholdSecondaryBssB --ccaEdThresholdPrimaryBssC=${ccaEdThresholdPrimaryBssC} --ccaEdThresholdSecondaryBssC=$ccaEdThresholdSecondaryBssC  --ccaEdThresholdPrimaryBssD=${ccaEdThresholdPrimaryBssD} --ccaEdThresholdSecondaryBssD=$ccaEdThresholdSecondaryBssD --ccaEdThresholdPrimaryBssE=${ccaEdThresholdPrimaryBssE} --ccaEdThresholdSecondaryBssE=$ccaEdThresholdSecondaryBssE --ccaEdThresholdPrimaryBssF=${ccaEdThresholdPrimaryBssF} --ccaEdThresholdSecondaryBssF=$ccaEdThresholdSecondaryBssF --ccaEdThresholdPrimaryBssG=${ccaEdThresholdPrimaryBssG} --ccaEdThresholdSecondaryBssG=$ccaEdThresholdSecondaryBssG --downlinkA=${downlinkA} --downlinkB=$downlinkB --downlinkC=$downlinkC --downlinkD=$downlinkD --downlinkE=$downlinkE --downlinkF=$downlinkF --downlinkG=$downlinkG --uplinkA=$uplinkA --uplinkB=$uplinkB --uplinkC=$uplinkC --uplinkD=${uplinkD} --uplinkE=$uplinkE --uplinkF=$uplinkF --uplinkG=$uplinkG --channelBondingType=$channelBondingType --n=$n --nBss=$nBss --rxSensitivity=$rxSensitivity --RngRun=$RngRun --result_filename=$result_filename" 
+wait
+done
+wait
+done
+wait
+done
