aa6762f90a50418c0d004ae721e8737d7f8c13df
TomokiNAKASHIMA <tomoki_nakasima@yahoo.co.jp>
2021-06-14 13:15:44 +0900

    make new branch

--- scratch/dynamicMCS4.sh
+++ scratch/dynamicMCS4.sh
@@ -1,26 +1,26 @@
 payloadSize=1472 # bytes
-simulationTime=10 # seconds
+simulationTime=100 # seconds
 distance=5 # meters
-interBssDistance=10 # meters
+interBssDistance=15 # meters
 txMaskInnerBandMinimumRejection=-40.0 # dBr
 txMaskOuterBandMinimumRejection=-56.0 # dBr
 txMaskOuterBandMaximumRejection=-80.0 # dBr
 
-channelBssA=1
+channelBssA=36
 channelBssB=36
-channelBssC=144
-channelBssD=1
-channelBssE=36
-channelBssF=144
-channelBssG=144
+channelBssC=52
+channelBssD=52
+channelBssE=44
+channelBssF=44
+channelBssG=44
 
-primaryChannelBssA=1
+primaryChannelBssA=36
 primaryChannelBssB=36
-primaryChannelBssC=144
-primaryChannelBssD=1
-primaryChannelBssE=36
-primaryChannelBssF=144
-primaryChannelBssG=144
+primaryChannelBssC=52
+primaryChannelBssD=52
+primaryChannelBssE=44
+primaryChannelBssF=44
+primaryChannelBssG=44
 
 mcs1=VhtMcs0
 mcs2=VhtMcs0
@@ -30,7 +30,7 @@ mcs5=VhtMcs0
 mcs6=VhtMcs0
 mcs7=VhtMcs0
 
-ccaSdThreshold=-62
+ccaSdThreshold=-82
 
 ccaEdThresholdPrimaryBssA=-62.0
 ccaEdThresholdSecondaryBssA=-62.0
@@ -47,38 +47,38 @@ ccaEdThresholdSecondaryBssF=-62.0
 ccaEdThresholdPrimaryBssG=-62.0
 ccaEdThresholdSecondaryBssG=-62.0
 
-downlinkA=0
+downlinkA=100
 downlinkB=100
 downlinkC=100
 downlinkD=100
-downlinkE=100
-downlinkF=100
+downlinkE=0
+downlinkF=0
 downlinkG=0
 
 
 uplinkA=0
 uplinkB=0
 uplinkC=0
-uplinkD=100
-uplinkE=100
-uplinkF=100
+uplinkD=0
+uplinkE=0
+uplinkF=0
 uplinkG=0
 
 channelBondingType="ConstantThreshold"
 Test=""
 
 n=10
-nBss=6
+nBss=4
 
 rxSensitivity=-100
 
-result_filename=result.csv
+result_filename=dynamicMCS4_100.csv
 
-for ccaSdThreshold in -82;
+for ccaSdThreshold in -82 -62 -83.63746694;
 do
-for mcs in VhtMcs0;
+for mcs in VhtMcs4;
 do
-for RngRun in 1 ;
+for RngRun in {1..100};
 do
 echo "Starting Bss=$nBss n=$n, ccaSdThreshold=$ccaSdThreshold, mcs=$mcs, distance=$distance "
 ../waf --run "channel-bonding  --payloadSize=$payloadSize --simulationTime=$simulationTime --distance=$distance --interBssDistance=$interBssDistance --txMaskInnerBandMinimumRejection=$txMaskInnerBandMinimumRejection --txMaskOuterBandMinimumRejection=$txMaskOuterBandMinimumRejection --txMaskOuterBandMaximumRejection=$txMaskOuterBandMaximumRejection --channelBssA=$channelBssA --primaryChannelBssA=$primaryChannelBssA --channelBssB=$channelBssB --primaryChannelBssB=$primaryChannelBssB --channelBssC=$channelBssC --primaryChannelBssC=$primaryChannelBssC --channelBssD=$channelBssD --primaryChannelBssD=$primaryChannelBssD --channelBssE=$channelBssE --primaryChannelBssE=$primaryChannelBssE --channelBssF=$channelBssF --primaryChannelBssF=$primaryChannelBssF --channelBssG=$channelBssG --primaryChannelBssG=$primaryChannelBssG --mcs1=$mcs --mcs2=$mcs --mcs3=$mcs --mcs4=$mcs --mcs5=$mcs --mcs6=$mcs --mcs7=$mcs --ccaSdThreshold=$ccaSdThreshold --ccaEdThresholdPrimaryBssA=${ccaEdThresholdPrimaryBssA} --ccaEdThresholdSecondaryBssA=$ccaEdThresholdSecondaryBssA --ccaEdThresholdPrimaryBssB=${ccaEdThresholdPrimaryBssB} --ccaEdThresholdSecondaryBssB=$ccaEdThresholdSecondaryBssB --ccaEdThresholdPrimaryBssC=${ccaEdThresholdPrimaryBssC} --ccaEdThresholdSecondaryBssC=$ccaEdThresholdSecondaryBssC  --ccaEdThresholdPrimaryBssD=${ccaEdThresholdPrimaryBssD} --ccaEdThresholdSecondaryBssD=$ccaEdThresholdSecondaryBssD --ccaEdThresholdPrimaryBssE=${ccaEdThresholdPrimaryBssE} --ccaEdThresholdSecondaryBssE=$ccaEdThresholdSecondaryBssE --ccaEdThresholdPrimaryBssF=${ccaEdThresholdPrimaryBssF} --ccaEdThresholdSecondaryBssF=$ccaEdThresholdSecondaryBssF --ccaEdThresholdPrimaryBssG=${ccaEdThresholdPrimaryBssG} --ccaEdThresholdSecondaryBssG=$ccaEdThresholdSecondaryBssG --downlinkA=${downlinkA} --downlinkB=$downlinkB --downlinkC=$downlinkC --downlinkD=$downlinkD --downlinkE=$downlinkE --downlinkF=$downlinkF --downlinkG=$downlinkG --uplinkA=$uplinkA --uplinkB=$uplinkB --uplinkC=$uplinkC --uplinkD=${uplinkD} --uplinkE=$uplinkE --uplinkF=$uplinkF --uplinkG=$uplinkG --channelBondingType=$channelBondingType --n=$n --nBss=$nBss --rxSensitivity=$rxSensitivity --RngRun=$RngRun --result_filename=$result_filename" 
